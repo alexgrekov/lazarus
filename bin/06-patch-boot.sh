ca9dc0e48695b6169fb08b23efaab602fb0f4c93
Александр Греков <aleksandr.grekov@edna.ru>
2026-02-20 19:34:43 +0300

    Add EFI boot support

--- bin/06-patch-boot.sh
+++ bin/06-patch-boot.sh
@@ -8,4 +8,12 @@ echo "sed 's|default vesamenu.c32|default install|g' --in-place _iso/isolinux/is
 
 sed 's|default vesamenu.c32|default install|g' --in-place _iso/isolinux/isolinux.cfg
 
+echo "Patching boot/grub/grub.cfg";
+
+echo "sed \"s|^insmod|set default='Install'\\ninsmod|\" --in-place _iso/boot/grub/grub.cfg;";
+sed "s|^insmod|set default='Install'\ninsmod|" --in-place _iso/boot/grub/grub.cfg;
+
+echo "sed \"s|^insmod|set timeout=3\\ninsmod|\" --in-place _iso/boot/grub/grub.cfg;";
+sed "s|^insmod|set timeout=3\ninsmod|" --in-place _iso/boot/grub/grub.cfg;
+
 echo "boot patch finished";
